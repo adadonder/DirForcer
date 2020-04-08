cd19c172d003d043880d0cefb8c510ab9324721a
adadonder <adadonderr@gmail.com>
2020-04-08 00:16:15 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DirForcer',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='adadonder',
+    author_email='adadonderr@gmail.com',
+    description=''
+)
